ea07de6a92ab7b529d33704fec6e5948935a465d
Opwondo <opwondow@gmail.com>
2026-01-18 01:27:07 +0300

    Fix URLs to match checker: add_book/ and edit_book/

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -26,10 +26,9 @@ urlpatterns = [
     path('member-dashboard/', views.member_view, name='member_dashboard'),
     path('dashboard/', views.redirect_dashboard, name='dashboard'),
     path('', lambda request: redirect('dashboard')),  # redirect root URL to dashboard
-     # Secured book URLs
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
 
 
 ]
