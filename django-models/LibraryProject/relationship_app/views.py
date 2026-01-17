3712eb59b8f10981fd4368a6d2b97fbb6cbbb36c
Opwondo <opwondow@gmail.com>
2026-01-17 22:29:44 +0300

    Advanced my Relationship App with FBV and CBV

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from bookshelf.models import Book  
 from relationship_app.models import Library  
 from django.views.generic import DetailView
-
+from .models import Library
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
